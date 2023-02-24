76c4d9479a87e097b9de0870c7d51ef6565ddd79
Ashton Kemerling <ashton.kemerling@protonmail.com>
2023-02-23 21:57:38 -0700

    Resume builds

--- flake.nix
+++ flake.nix
@@ -1,11 +1,38 @@
 {
-  description = "A very basic flake";
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  description = "LaTeX Resume";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
+    flake-utils.url = github:numtide/flake-utils;
   };
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib; eachSystem allSystems (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-tetex latex-bin latexmk etoolbox
+            xcolor url fancyhdr tools xargs xkeyval microtype bigfoot lipsum
+            geometry hyperref pdftexcmds infwarerr kvoptions epstopdf;
+      };
+    in rec {
+      packages = {
+        document = pkgs.stdenvNoCC.mkDerivation rec {
+          name = "resume";
+          src = self;
+          buildInputs = [ pkgs.coreutils tex ];
+          phases = ["unpackPhase" "buildPhase" "installPhase"];
+          buildPhase = ''
+            export PATH="${pkgs.lib.makeBinPath buildInputs}";
+            mkdir -p .cache/texmf-var
+            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+              latexmk -interaction=nonstopmode -pdf -lualatex \
+              resume.tex
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp resume.pdf $out/
+          '';
+        };
+      };
+      defaultPackage = packages.document;
+    });
 }
